09fc451505ae12c595382b1de6de4f65a67e3cf9
Yuri Duque <yurithielmann83@gmail.com>
2022-10-07 01:52:33 -0300

    update custom response

--- src/utils/customResponse.ts
+++ src/utils/customResponse.ts
@@ -1,24 +1,67 @@
 import { Request, Response, NextFunction } from 'express';
 
+enum STATUS {
+  success = 200,
+  error = 500,
+  badRequest = 400,
+  unauthorized = 401,
+}
+
 export default function responseMiddleware(
   request: Request,
   response: Response,
   next: NextFunction
 ) {
   response.success = (message: string, data?: any) => {
-    return response.status(200).send({ message, data });
+    const body = {
+      message,
+      data,
+    };
+    return buildResponse(response, STATUS.success, body);
   };
 
   response.error = (message: string, data?: any, error?: Error) => {
-    return response.status(500).send({ message, data, error: error?.message });
+    const body = {
+      message,
+      data,
+      error,
+    };
+    return buildResponse(response, STATUS.error, body);
   };
 
   response.badRequest = (message: string, data?: any, error?: Error) => {
-    return response.status(400).send({ message, data, error: error?.message });
+    const body = {
+      message,
+      data,
+      error,
+    };
+    return buildResponse(response, STATUS.badRequest, body);
   };
 
   response.unauthorized = (message: string, data?: any) => {
-    return response.status(401).send({ message, data });
+    const body = {
+      message,
+      data,
+    };
+    return buildResponse(response, STATUS.unauthorized, body);
   };
+
   next();
+}
+
+function buildResponse(response: Response, status: STATUS, body: any) {
+  log(status, body);
+  return response.status(status).send(body);
+}
+
+function log(status: STATUS, body: any) {
+  const statusName = STATUS[status];
+
+  const logMessage = `[Response] - ${statusName}: ${JSON.stringify(body)}`;
+
+  if (status === STATUS.success) {
+    console.log(logMessage);
+  } else {
+    console.error(logMessage);
+  }
 }
